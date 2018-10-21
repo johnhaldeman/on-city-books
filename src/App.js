6b8d19b132b4fe9e4226eefd12296869561a8947
John Haldeman <john.haldeman@infoinsightsllc.com>
2018-10-20 21:36:00 -0400

    Start App Interface Prototype

--- src/App.js
+++ src/App.js
@@ -2,25 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, DropdownButton, Dropdown, Row, Badge, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar bg="dark" variant="dark" collapseOnSelect expand="md">
+          <Navbar.Brand href="#home">
+            ON City Books
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Navbar.Text>
+              Visualizations for Ontario Municipal Financial Data
+            </Navbar.Text>
+          </Navbar.Collapse>          
+          <Navbar.Collapse className="justify-content-end">
+            <DropdownButton variant="danger" id="dropdown-basic-button" title="Choose a Financial Item">
+              <Dropdown.Item href="#/Revenues">Revenues</Dropdown.Item>
+              <Dropdown.Item href="#/Expenses">Expenses</Dropdown.Item>
+              <Dropdown.Item href="#/Deficits">Deficits/Surpluses</Dropdown.Item>
+            </DropdownButton>;
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Container fluid="true">
+          <Row>
+            <Col>
+              <div>
+                <Badge variant="primary">Windsor</Badge>
+                <Badge variant="success">Toronto</Badge>
+                <Badge variant="danger">Ottawa</Badge>
+                <Badge variant="warning">London</Badge>
+                <Badge variant="info">Kingston</Badge>
+                <Badge variant="dark">Thunder Bay</Badge>
+              </div>
+            </Col>
+          </Row>
+
+        </Container>
       </div>
     );
   }
