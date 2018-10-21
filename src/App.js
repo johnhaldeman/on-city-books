9aafb8cb6706deee0a8ef420bd538401fca17cc0
John Haldeman <john.haldeman@infoinsightsllc.com>
2018-10-21 08:54:34 -0400

    Prototype experiments with City Lists

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, DropdownButton, Dropdown, Row, Badge, Col } from 'react-bootstrap';
+import { Navbar, Container, DropdownButton, Dropdown, Row, Badge, Col, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -20,24 +20,23 @@ class App extends Component {
           </Navbar.Collapse>          
           <Navbar.Collapse className="justify-content-end">
             <DropdownButton variant="danger" id="dropdown-basic-button" title="Choose a Financial Item">
-              <Dropdown.Item href="#/Revenues">Revenues</Dropdown.Item>
+              <Dropdown.Item active href="#/Revenues">Revenues</Dropdown.Item>
               <Dropdown.Item href="#/Expenses">Expenses</Dropdown.Item>
               <Dropdown.Item href="#/Deficits">Deficits/Surpluses</Dropdown.Item>
-            </DropdownButton>;
+            </DropdownButton>
           </Navbar.Collapse>
         </Navbar>
 
-        <Container fluid="true">
+        <Container fluid={true}>
           <Row>
             <Col>
-              <div>
-                <Badge variant="primary">Windsor</Badge>
-                <Badge variant="success">Toronto</Badge>
-                <Badge variant="danger">Ottawa</Badge>
-                <Badge variant="warning">London</Badge>
-                <Badge variant="info">Kingston</Badge>
-                <Badge variant="dark">Thunder Bay</Badge>
-              </div>
+              <Badge variant="primary" pill >&times; Windsor</Badge>
+              <Badge variant="success" pill >&times; Toronto</Badge>
+              <Badge variant="danger" pill >&times; Ottawa</Badge>
+              <Badge variant="warning" pill >&times; London</Badge>
+              <Badge variant="info" pill >&times; Kingston</Badge>
+              <Badge variant="dark" pill >&times; Thunder Bay</Badge>
+              &nbsp;&nbsp;&nbsp;&nbsp;<Badge variant="primary" pill >+ Add New City</Badge>
             </Col>
           </Row>
 
