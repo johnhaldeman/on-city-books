826349735ebbb5e78bf87ae40c650a9bf296d1ec
John Haldeman <john.haldeman@infoinsightsllc.com>
2018-10-23 21:48:05 -0400

    Continue prototyping

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, DropdownButton, Dropdown, Row, Badge, Col, Button } from 'react-bootstrap';
 
 class App extends Component {
+  cityData = [
+    {
+      city: "Windsor",
+      revenue_streams: [
+        { description: "Property Taxes & Payments-In-Lieu of Taxes", calculation: "slc.10X.L9940.C01.01", value: 307577881},
+        { description: "Grants from Other Levels of Government", calculation: "slc.10X.L0899.C01.01 + slc.10X.L0699.C01.01", value: 215492558},
+        { description: "Total user fees and service charges (SLC 12 9910 04)", calculation: "slc.10X.L1299.C01.01", value: 124692455},
+        { description: "Other Revenues", calculation: "slc.10X.L9910.C01.01 - slc.10X.L9940.C01.01 - slc.10X.L0899.C01.01 - slc.10X.L0699.C01.01 - slc.10X.L1299.C01.01", value: 124920913},
+        { description: "Total Revenues", calculation: "slc.10X.L9910.C01.01", value: 772683807},
+      ]
+    }
+  ];
+
+  constructor(props) {
+    super(props);
+
+
+  }
+
+
   render() {
     return (
       <div>
