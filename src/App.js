6ebd7540149b3c2b77d12dafb1ce6448d65f3dba
John Haldeman <john.haldeman@infoinsightsllc.com>
2018-11-16 22:37:45 -0500

    add additional data set to stacked bars

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import { StackedBarChart } from './StackedBarChart';
 
 class App extends Component {
 
+  
+  years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016];
+
   cityData = [
     {
       city: "Windsor",
-      years:  [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
       revenue_streams: [
         { description: "Property Taxes & Payments-In-Lieu of Taxes", calculation: "slc.10X.L9940.C01.01", value: 307577881},
         { description: "Grants from Other Levels of Government", calculation: "slc.10X.L0899.C01.01 + slc.10X.L0699.C01.01", value: 215492558},
@@ -36,8 +38,19 @@ class App extends Component {
         { description: "Grants from Other Levels of Government", calculation: "slc.10X.L0899.C01.01 + slc.10X.L0699.C01.01", value: 307577881},
         { description: "Total user fees / service charges", calculation: "slc.10X.L1299.C01.01", value: 124692455},
         { description: "Other Revenues", calculation: "slc.10X.L9910.C01.01 - slc.10X.L9940.C01.01 - slc.10X.L0899.C01.01 - slc.10X.L0699.C01.01 - slc.10X.L1299.C01.01", value: 124920913}
+      ],
+      year_data: [
+        { year: 2009, prop_taxes: 246781231.40392, grants: 250717859.082196, user_fees: 84022372.1169149, other: 123196483.115626, total: 704717945.718657},
+        { year: 2010, prop_taxes: 337378912.878514, grants: 179827406.885892, user_fees: 150972248.348879, other: 158329506.839765, total: 826508074.95305},
+        { year: 2011, prop_taxes: 235607572.225583, grants: 267362230.782504, user_fees: 110533416.711777, other: 109355670.172794, total: 722858889.892659},
+        { year: 2012, prop_taxes: 289379862.207876, grants: 242543598.95452, user_fees: 157926925.636544, other: 99489534.4582651, total: 789339921.257206},
+        { year: 2013, prop_taxes: 442880644.813725, grants: 272639038.710534, user_fees: 89919366.8477934, other: 154406083.082713, total: 959845133.454766},
+        { year: 2014, prop_taxes: 308948515.508727, grants: 251712181.067756, user_fees: 130864861.305018, other: 88203910.6290533, total: 779729468.510555},
+        { year: 2015, prop_taxes: 261891597.260469, grants: 215762193.837151, user_fees: 158171798.444655, other: 150977078.426298, total: 786802667.968573},
+        { year: 2016, prop_taxes: 204316255.183416, grants: 156872407.55885, user_fees: 101381657.702847, other: 122926590.702981, total: 585496911.148094}
+
       ]
-    },
+    }/*,
     {
       city: "Sarnia",
       revenue_streams: [
@@ -73,7 +86,7 @@ class App extends Component {
         { description: "Total user fees / service charges", calculation: "slc.10X.L1299.C01.01", value: 124692455},
         { description: "Other Revenues", calculation: "slc.10X.L9910.C01.01 - slc.10X.L9940.C01.01 - slc.10X.L0899.C01.01 - slc.10X.L0699.C01.01 - slc.10X.L1299.C01.01", value: 124920913}
       ]
-    }
+    }*/
   ];
 
   colours = [
@@ -165,7 +178,7 @@ class App extends Component {
 
           <Row>
             <Col>
-              <StackedBarChart data={this.cityData[0]}></StackedBarChart>
+              <StackedBarChart data={this.cityData} years={this.years}></StackedBarChart>
             </Col>
           </Row>
 
--- src/StackedBarChart.js
+++ src/StackedBarChart.js
@@ -7,61 +7,89 @@ export class StackedBarChart extends Component {
         super(props);
         this.chartRef = React.createRef();
 
-        let colorProfile = d3.schemeGreens[props.data.revenue_streams.length + 1];
 
         this.state = {
             data: props.data,
-            colorProfile: colorProfile
+            years: props.years
         }
 
     }
 
     componentDidMount(){
+
+        let numSeries = this.state.data[0].revenue_streams.length + 1;
+        let colorProfiles = [
+            d3.schemeGreens[numSeries],
+            d3.schemeOranges[numSeries]
+        ];
+
         let svg = d3.select(this.chartRef.current);
         let width = svg.style("width").replace("px", "");
-        svg.attr("height", width);
+        svg.attr("height", width / 2);
 
 
         let stacker = d3.stack().keys(["prop_taxes", "grants", "user_fees", "other"]);
 
-        let totals = this.state.data.year_data.map(function(d){
-            return d.total;
-        })
-        let max = d3.max(totals);
+        let alltotals = [];
+        
+        for(let i in this.state.data){
+            let totals = this.state.data[i].year_data.map(function(d){
+                return d.total;
+            })
+            alltotals = alltotals.concat(totals);
+        }
+        let max = d3.max(alltotals);
 
-        let stackedData = stacker(this.state.data.year_data);
+        let stacks = [];
+        for(let i in this.state.data){
+            let stackArr = stacker(this.state.data[i].year_data);
+            stacks.push(stackArr);
+        }
+
+        let xDomain = [];
+        for (let i in this.state.years){
+            for(let j in this.state.data){
+                xDomain.push(this.state.years[i] + "_" + j);
+            }
+        }
 
         let xScale = d3.scaleBand()
-            .domain(this.state.data.years)
+            .domain(xDomain)
             .rangeRound([0, width])
             .padding(0.08);
 
         
         let yScale = d3.scaleLinear()
             .domain([0, max])
-            .range([width, 0]);
+            .range([width / 2, 0]);
         
         
-        let years = this.state.data.years;
-        let color = this.state.colorProfile;
-        svg.append("g")
-            .selectAll("g")
-            .data(stackedData)
-            .enter().append("g")
-              .attr("fill", function(d, i){
-                  return color[i]
-            })
-            .selectAll("rect")
-            .data(d => d)
-            .enter().append("rect")
-              .attr("x", (d, i) => xScale(years[i]))
-              .attr("y", d => {
-                  return yScale(d[1]);
-              })
-              .attr("height", d => {
-                  return yScale(d[0]) - yScale(d[1]);
-              })
-              .attr("width", xScale.bandwidth());
+        let years = this.state.years;
+        let colors = colorProfiles;
+
+        for(let stacknum in stacks){
+            svg.append("g")
+               .selectAll("g")
+               .data(stacks[stacknum])
+               .enter().append("g")
+               .attr("fill", function(d, i){
+                   return colors[stacknum][i]
+               })
+               .selectAll("rect")
+               .data(d => d)
+               .enter().append("rect")
+               .attr("x", (d, i) => {
+                   return xScale(years[i] + "_" + stacknum)
+               })
+               .attr("y", d => {
+                   return yScale(d[1]);
+               })
+               .attr("height", d => {
+                   return yScale(d[0]) - yScale(d[1]);
+               })
+               .attr("width", xScale.bandwidth());
+        }
+        
         
         // svg.append("g")
         //      .call(xAxis);
