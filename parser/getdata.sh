62cf397f0a02be674071603c6653ce0be62ed6da
John Haldeman <john.haldeman@infoinsightsllc.com>
2018-11-28 19:51:35 -0500

    Start work on data retrieval and parsing

---
+++ parser/getdata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2017.zip
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2016.zip
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2015.zip
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2014.zip
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2013.zip
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2012.zip
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2011.zip
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2010.zip
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2009.zip
+
+unzip *.zip
