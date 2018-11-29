477a47d6f7e9e3ab4c0bdb49fb6ba03d8b1b3bb0
John Haldeman <john.haldeman@infoinsightsllc.com>
2018-11-28 19:59:58 -0500

    Move getdata script to data directory

--- data/getdata.sh
+++ data/getdata.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm *.zip*
+
 wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2017.zip
 wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2016.zip
 wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2015.zip
