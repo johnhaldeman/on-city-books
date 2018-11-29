b5ba1eb290fae1fd7521ff1dd383a845d71f3f99
John Haldeman <john.haldeman@infoinsightsllc.com>
2018-11-28 19:56:09 -0500

    fix unzip command - required a find

--- parser/getdata.sh
+++ parser/getdata.sh
@@ -10,4 +10,4 @@ wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, li
 wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2010.zip
 wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36" https://efis.fma.csc.gov.on.ca/fir/MultiYearReport/fir_data_2009.zip
 
-unzip *.zip
+find *.zip -exec unzip {} \;
