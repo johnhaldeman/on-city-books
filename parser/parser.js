5ece0d48aafb2bd4a9ee12ea5e1358731fb26e98
John Haldeman <john.haldeman@infoinsightsllc.com>
2018-12-04 21:14:55 -0500

    start building municipal data objects in parser

--- parser/parser.js
+++ parser/parser.js
@@ -2,7 +2,8 @@ let parse = require('csv-parse/lib/sync');
 let fs = require('fs');
 let path = require( 'path' );
 
-let fileDir = "data"
+let fileDir = "data";
+let outDir = path.join("data", "out");
 
 fs.readdir( fileDir, function( err, files ) {
     if( err ) {
@@ -10,11 +11,57 @@ fs.readdir( fileDir, function( err, files ) {
         process.exit( 1 );
     } 
 
-    files.forEach( function( file, index ) {
-            var location = path.join( fileDir, file );
+    let munis = {};
 
-            console.log(location);
+    files.forEach( function( file, index ) {
+        var location = path.join( fileDir, file );
+
+        if(file.endsWith(".csv")){
+            console.log("Processing: " + location);
+            let data = fs.readFileSync(location).toString();
+            let records = parse(data, {columns: true});
+
+            for(let recordnum in records){
+                let record = records[recordnum];
+
+                let id = record.MUNID.trim();
+                if(munis[id] === undefined){
+                    munis[id] = {
+                        id: id,
+                        desc: record.MUNICIPALITY_DESC
+                    };
+                }
+                let muni = munis[id];
+
+                let MARSYEAR = record.MARSYEAR;
+                if(muni[MARSYEAR] === undefined){
+                    muni[MARSYEAR] = {};
+                }
+                let muniyear = muni[MARSYEAR];
+
+
+                let SLC = record.SLC;
+                muniyear[SLC] = {
+                    amount: Number(record.AMOUNT),
+                    value_text: record.VALUE_TEXT,
+                    line_desc: record.SCHEDULE_LINE_DESC,
+                    column_desc: record.SCHEDULE_COLUMN_DESC
+                }
+            }
+        }
     } );
+
+    for(let id in munis){
+        let muni = munis[id];
+
+        fs.writeFile(fs.join(outDir, id + ".json"), JSON.stringify(muni), function(err, data){
+            if (err) console.log(err);
+            console.log("Successfully Written to " + id + ".json");
+        });
+
+    }
+
+
 } );
 
 
