2915853d7b21716f2db578a3346bc969fb5a1968
John Haldeman <john.haldeman@infoinsightsllc.com>
2019-02-12 21:56:41 -0500

    Clean up table generation in modal

--- src/CitySelectionModal.js
+++ src/CitySelectionModal.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Modal } from 'react-bootstrap';
 
 function getHeaders(muniData) {
-    let retData = [<th>id</th>];
+    let retData = [<th key="id">id</th>];
     if(muniData !== undefined){
         for(let header in muniData[1000]){
-            retData.push(<th>{header}</th>);
+            retData.push(<th key={header}>{header}</th>);
         }
     }
     return retData;
@@ -15,11 +15,11 @@ function getData(muniData) {
     let retData = [];
     if(muniData !== undefined){
         for(let muniID in muniData){
-            let row = [<td>{muniID}</td>];
+            let row = [<td key="id">{muniID}</td>];
             for(let item in muniData[muniID]){
-                row.push(<td>{muniData[muniID][item]}</td>);
+                row.push(<td key={muniID + item}>{muniData[muniID][item]}</td>);
             }
-            retData.push(<tr>{row}</tr>)
+            retData.push(<tr key={muniID}>{row}</tr>)
         }
     }
     return retData;
@@ -38,12 +38,15 @@ export function CitySelectionModal(props){
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-
-                <table>
-                    <tr>
-                        {getHeaders(props.muniData)}
-                    </tr>
-                    {getData(props.muniData)}
+                <table className="table table-striped table-sm table-hover table-100pc">
+                    <thead>
+                        <tr>
+                            {getHeaders(props.muniData)}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {getData(props.muniData)}
+                    </tbody>
                 </table>
             </Modal.Body>
         </Modal>
